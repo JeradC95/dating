202ef0d95045deaf8c61834946916da8f77d44ef
Jerad <Jclark74@mail.greenriver.edu>
2021-02-25 16:04:03 -0500

    added model with data layer and validation

---
+++ model/data-layer.php
@@ -0,0 +1,17 @@
+<?php
+
+function getIndoorInterests(){
+    return array("tv", "gaming", "movies", "coodking", "reading", "art");
+}
+
+function getOutdoorInterests(){
+    return array("cycling", "hiking", "camping", "animals", "swimming", "running");
+}
+
+function getGender(){
+    return array("male", "female");
+}
+
+function getState(){
+    return array("Washington", "Oregon", "California");
+}
